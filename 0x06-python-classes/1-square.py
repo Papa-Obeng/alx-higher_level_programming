2034d70099a3475654afd6da5a8d0aa7673ddded
Papa-Obeng <papaobengn@gmail.com>
2023-04-12 15:08:27 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """This class represents a square"""
+
+
+    def __init__(self, size):
+        """Args: size - represents the size of the sqaure defined"""
+
+
+        self.__size = size
